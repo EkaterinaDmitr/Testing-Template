f1af1bba98f72e854a5f8c8faeda37d4576b27ba
EkaterinaDmitr <ketrins-lite_94@mail.ru>
2022-12-07 01:33:01 +0300

    тестовое задание почти выполнено

--- source/js/main.js
+++ source/js/main.js
@@ -27,6 +27,72 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+
+document.querySelector('.burger').addEventListener('click', function () {
+  document.querySelector('.burger span').classList.toggle('active');
+  document.querySelector('.menu__list').classList.toggle('animate');
+  document.querySelector('body').classList.toggle('open-menu');
+});
+
+
+function appearCountries() {
+  const countryCircles = document.querySelectorAll('.circle__country');
+  const orbit = document.querySelector('.illustration__circle');
+  const countries = ['1', '2', '3', '4', '5'];
+  const renderCountries = new Promise((resolve) => {
+    setTimeout(() => {
+      countryCircles.forEach((item, i) => {
+        setTimeout(() => {
+          item.classList.add(`circle__country--active--${countries[i]}`);
+          // eslint-disable-next-line no-unused-expressions
+          i === countries.length - 1 ? resolve() : null;
+        }, i * 400);
+      });
+    }, 3000);
+  });
+
+  renderCountries.then(() => {
+    setTimeout(() => {
+      orbit.style.animation = 'rotation 20s linear infinite';
+      countryCircles.forEach((item) => {
+        item.style.animation = 'rotationBack 20s linear infinite';
+      });
+    }, 200);
+  });
+}
+
+appearCountries();
+
+
+const MIN_NAME_LENGTH = 4;
+const MAX_NAME_LENGTH = 12;
+
+let stopSumb = (/[!@#$%^&*()]/);
+
+
+let searcHeaderInput = document.querySelector('input');
+
+searcHeaderInput.addEventListener('input', () => {
+  const valueLength = searcHeaderInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    searcHeaderInput.setCustomValidity('Ещё' + ' ' + (MIN_NAME_LENGTH - valueLength) + ' симв.');
+
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    searcHeaderInput.setCustomValidity('Удалите лишнее' + (valueLength - MAX_NAME_LENGTH) + 'симв.');
+
+  } else {
+    searcHeaderInput.setCustomValidity('');
+  }
+
+  searcHeaderInput.reportValidity();
+
+});
+
+searcHeaderInput.addEventListener('input', function () {
+  this.value = this.value.replace(stopSumb, '');
+});
+
 // ---------------------------------
 
 // ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
